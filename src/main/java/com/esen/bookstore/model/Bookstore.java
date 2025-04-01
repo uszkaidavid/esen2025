7785ed533aa85314c301ce3a6de89b3422af14c6
RolandMajor <major.sandor@inf.unideb.hu>
2025-04-01 09:33:56 +0200

    Configure database + added entities

---
+++ src/main/java/com/esen/bookstore/model/Bookstore.java
@@ -0,0 +1,19 @@
+package com.esen.bookstore.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Bookstore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String location;
+    private Double priceModifier;
+    private Double moneyInCashRegister;
+
+}
